371eb26671fe3a92a262abb168139d13ae630fbf
stouzaki <stouzaki@system.work>
2021-04-27 19:02:23 +0900

    a

--- js/index.js
+++ js/index.js
@@ -1,48 +1,50 @@
 /** 初期表示*/
 $(function () {
-    // 今年のページを取得して表示する
-    getSchedulePage(2021);
+  // 今年のページを取得して表示する
+  getSchedulePage(2021);
 
-    // 年切替イベント
-    $("#yearSelect").on("change", (e) => onChageYearSelect(e));
+  // 年切替イベント
+  $("#yearSelect").on("change", (e) => onChageYearSelect(e));
 });
 
 /** 年切替 */
 function onChageYearSelect(e) {
-    let year = $(e.currentTarget).val();
-    getSchedulePage(year);
+  let year = $(e.currentTarget).val();
+  getSchedulePage(year);
 }
 
 /** Noを降順にセットする*/
 function setNo() {
-    let elemNos = $(".no");
-    let index = 0;
-    for (let no = elemNos.length; 0 < no; no--) {
-        $(elemNos[index]).html(`No.${no}`);
-        index++;
-    }
+  let elemNos = $(".no");
+  let index = 0;
+  for (let no = elemNos.length; 0 < no; no--) {
+    $(elemNos[index]).html(`No.${no}`);
+    index++;
+  }
 }
 
 /** スケジュールページを取得 */
 function getSchedulePage(year) {
-    $.ajax({
-        type: 'GET',
-        url: `./page/schedule/${year}.html`,
-        dataType: 'html',
-        beforeSend: function () {
-            let $main = $('#main');
-            $main.html("Loading...");
-        },
-        success: function (data) {
-            let $main = $('#main');
-            $main.hide();
-            $main.html("");
-            $main.append(data);
-            setNo();
-            $main.fadeIn(1000);
-        },
-        error: function () {
-            alert('エラー');
-        }
-    });
+  let now = new Date();
+
+  $.ajax({
+    type: "GET",
+    url: `./page/schedule/${year}.html?${now.getTime}_${now.getSeconds}`,
+    dataType: "html",
+    beforeSend: function () {
+      let $main = $("#main");
+      $main.html("Loading...");
+    },
+    success: function (data) {
+      let $main = $("#main");
+      $main.hide();
+      $main.html("");
+      $main.append(data);
+      setNo();
+      $main.fadeIn(1000);
+    },
+    error: function () {
+      alert("エラー");
+    },
+  });
 }
