e6d0da0b16c9df2146125f6842c4212c93ddb873
東崎 将大 <shota_tozaki@mjs.co.jp>
2021-04-27 13:20:19 +0900

    a

--- js/index.js
+++ js/index.js
@@ -1,17 +1,15 @@
-window.addEventListener('load', init);
-
 /** 初期表示*/
-function init(e) {
+$(function () {
     setNo();
-}
+});
+
 
 /** Noを降順にセットする*/
 function setNo() {
-    let elemNos = document.querySelectorAll(".no");
+    let elemNos = $(".no");
     let index = 0;
     for (let no = elemNos.length; 0 < no; no--) {
-        const ele = elemNos[index];
-        ele.textContent = `No.${no}`;
+        $(elemNos[index]).html(`No.${no}`);
         index++;
     }
 }
